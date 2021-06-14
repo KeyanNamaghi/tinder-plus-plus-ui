b832a29afca0ad0c2b91a4c0d85dd6a6a327eca4
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2021-06-14 17:27:49 +0100

    basic working saga - does not save to state

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -2,14 +2,29 @@ import { call, put, takeEvery } from 'redux-saga/effects'
 
 const delay = (ms) => new Promise((res) => setTimeout(res, ms))
 
+const makeRequest = async () => {
+  const url = 'https://go-rest-image.herokuapp.com/image'
+  const res = await fetch(url)
+
+  // WCS doesn't always return JSON, need to catch any errors
+  const payload = await res.json().catch(() => {})
+
+  if (!res.ok) {
+    throw payload
+  }
+
+  return payload
+}
+
 // Worker saga will be fired on USER_FETCH_REQUESTED actions
 function* fetchUser(action) {
   try {
     console.log('saga time')
     yield delay(1000)
 
-    // const user = yield call(Api.fetchUser, action.payload.userId)
-    yield put({ type: 'USER_FETCH_SUCCEEDED', payload: { message: 'Keyan' } })
+    const user = yield call(makeRequest)
+    console.log(user)
+    yield put({ type: 'USER_FETCH_SUCCEEDED', payload: { message: user } })
   } catch (e) {
     // yield put({ type: 'USER_FETCH_FAILED', message: e.message })
   }
