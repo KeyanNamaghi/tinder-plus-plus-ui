31eaae94b7f714406a05a4af9a235d04ac2df330
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2021-06-26 01:02:14 +0100

    working swipe

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from '../Image/Image'
 
+/*
+0 - active 
+1 - behind
+2 - offscreen
+*/
+
 const Stack = () => {
-  return (
-    <div style={{ display: 'grid' }}>
-      <Image primary />
-      <Image />
-    </div>
-  )
+  const [current, setCurrent] = useState(0)
+
+  const images = [
+    <Image index={0} current={current} toggle={setCurrent} />,
+    <Image index={1} current={current} toggle={setCurrent} />,
+    <Image index={2} current={current} toggle={setCurrent} />,
+    <Image index={3} current={current} toggle={setCurrent} />
+  ]
+
+  return <div style={{ display: 'grid' }}>{images}</div>
 }
 
 export default Stack
--- src/components/image/Image.jsx
+++ src/components/image/Image.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useSpring, animated } from '@react-spring/web'
 import { useDrag } from 'react-use-gesture'
@@ -6,23 +6,40 @@ import logo from '../../logo.svg'
 import { LikeText, PassText } from '../ImageText/ImageText'
 import './Image.css'
 
-const Image = ({ primary = false }) => {
+const Image = ({ current, toggle, index }) => {
   const { images } = useSelector((state) => state.stockImages)
   const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
+
   const [likeActive, setLikeActive] = useState(false)
   const [passActive, setPassActive] = useState(false)
   const [superActive, setSuperActive] = useState(false)
   const inputRef = useRef()
 
-  const bind = useDrag(({ down, movement: [mx, my], cancel }) => {
-    api.start({ x: down ? mx : 0, y: down ? my : 0 })
+  useEffect(() => {
+    if (index === (current + 2) % 4) {
+      console.log({ current, index })
+      x.start(0)
+    }
+  }, [current])
+
+  const bind = useDrag(({ active, down, movement: [mx, my], direction: [xDir] }) => {
+    api.start(() => {
+      const isGone = mx > 300 && !active
+
+      if (isGone) {
+        toggle((index + 1) % 4)
+      }
+
+      const x = isGone ? 200 + window.innerWidth : active ? mx : 0
+      return { x: x, y: down ? my : 0 }
+    })
 
     if (down) {
-      if (mx > 50) {
+      if (mx > 0) {
         setLikeActive(true)
         setPassActive(false)
       }
-      if (mx < -50) {
+      if (mx < 0) {
         setPassActive(true)
         setLikeActive(false)
       }
@@ -33,11 +50,18 @@ const Image = ({ primary = false }) => {
   })
 
   return (
-    <animated.div ref={inputRef} {...bind()} style={{ x, y }} className={`Image-container Image-primary`}>
+    <animated.div
+      ref={inputRef}
+      {...bind()}
+      style={{ x, y }}
+      className={`Image-container ${current === index ? 'Image-primary' : ''} ${
+        current === (index + 2) % 4 ? 'Image-hidden' : `Image-${index}`
+      }`}
+    >
       <div className="Image">
         <LikeText active={likeActive} />
         <PassText active={passActive} />
-        <img src={images[primary ? 0 : 1]?.url || logo} className="Image Image-element" alt="logo" />
+        <img src={images[index]?.url || logo} className="Image Image-element" alt="logo" />
       </div>
     </animated.div>
   )
