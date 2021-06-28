31d2180a36225ada6797e6021dd4756901f52d2e
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2021-06-28 18:39:16 +0100

    :rocket: what is going on

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Image from '../Image/Image'
+// import Image from '../Image/Image'
 
 const Stack = () => {
-  const images = [<Image index={0} />, <Image index={1} />, <Image index={2} />, <Image index={3} />]
-
+  //   const images = [<Image index={0} />, <Image index={1} />, <Image index={2} />, <Image index={3} />]
+  const images = 'Images'
   return <div style={{ display: 'grid' }}>{images}</div>
 }
 
