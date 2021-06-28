6c8d82aa516f4ad23499d7a0cd5fe162692b997f
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2021-06-28 20:27:39 +0100

    normal imports

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image from 'components/Image/Image'
+import Image from '../Image/Image'
 
 const Stack = () => {
   const images = [<Image index={0} />, <Image index={1} />, <Image index={2} />, <Image index={3} />]
