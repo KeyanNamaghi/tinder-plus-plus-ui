d9527fdad8104969ffa8a72fb3f430597a944f99
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2021-06-28 21:17:31 +0100

    seriously it wont work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { FETCH_STOCK_IMAGES_REQUEST, LIKE_CURRENT_CARD_REQUEST } from './actions'
-import { LikeButton, PassButton, SuperButton } from './components/Buttons'
+import { LikeButton } from './components/Buttons/LikeButton'
+import { PassButton } from './components/Buttons/PassButton'
+import { SuperButton } from './components/Buttons/SuperButton'
 import Stack from './components/Stack/Stack'
 import './App.css'
 
