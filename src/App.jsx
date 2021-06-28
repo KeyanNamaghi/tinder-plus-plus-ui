b7216d6796ed8657df97b6a0d20403b80a2fd52f
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2021-06-28 20:42:25 +0100

    remove index

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { FETCH_STOCK_IMAGES_REQUEST, LIKE_CURRENT_CARD_REQUEST } from './actions'
-import { LikeButton, PassButton, SuperButton } from './components/Buttons/index'
+import { LikeButton, PassButton, SuperButton } from './components/Buttons'
 import Stack from './components/Stack/Stack'
 import './App.css'
 
