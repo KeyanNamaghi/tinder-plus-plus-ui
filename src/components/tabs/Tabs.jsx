4b5780e9faffb9fc835907ac8061b27c45221042
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2021-07-10 18:47:31 +0100

    :mute: removed logging

--- src/components/tabs/Tabs.jsx
+++ src/components/tabs/Tabs.jsx
@@ -16,11 +16,8 @@ export const Tabs = ({ position = 0, length }) => {
 
 export const useTabs = (length) => {
   const [position, setPosition] = useState(0)
-
   const left = () => setPosition(position > 0 ? position - 1 : 0)
   const right = () => setPosition(position < length - 1 ? +position + 1 : length - 1)
-
-  console.log({ length, position, left, right })
 
   return { position, left, right }
 }
